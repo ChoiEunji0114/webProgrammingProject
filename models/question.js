0fb3d91e64a2c94bd59c698d4cd33f90fcfd3eaf
eunji <eunji@eunjiui-MacBook-Pro.local>
2018-11-17 19:53:11 +0900

    first commit

---
+++ models/question.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+const Schema = mongoose.Schema;
+
+var schema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  title: {type: String, trim: true, required: true},
+  content: {type: String, trim: true, required: true},
+  
+  // 추가
+  sponser : {type:String, trim:true, required:true},
+  field : {type:String, trim:true, required:true},
+  applicant : {type:String, trim:true, required:true},
+  period : {type:String, trim:true, required:true},
+  manager : {type:String, trim:true, required:true},
+  tel : {type:String, trim:true, required:true},
+  
+  tags: [String],
+  numLikes: {type: Number, default: 0},
+  numAnswers: {type: Number, default: 0},
+  numReads: {type: Number, default: 0},
+  createdAt: {type: Date, default: Date.now}
+}, {
+  toJSON: { virtuals: true},
+  toObject: {virtuals: true}
+});
+schema.plugin(mongoosePaginate);
+var Question = mongoose.model('Question', schema);
+
+module.exports = Question;
